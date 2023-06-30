21e8565334b1de4296445cd59560c896b9ebdff0
Gaurav2002Kumawat <gauravkumawat830@gmail.com>
2023-06-30 17:16:27 +0530

    Updated the variableChanger() function in elementStore.js

--- elementStore.js
+++ elementStore.js
@@ -1,19 +1,26 @@
 ("use strict");
 var v1, v2, v3, v4, v5;
-// exporting these variables so that they can be accessed to the dataFetch.js
+var v6;
 function variableChanger(pageName) {
   if (pageName === ".destination") {
-    v1 = document.getElementsByClassName("destination--title");
-    v2 = document.getElementsByClassName("destination--description");
-    v3 = document.getElementsByClassName("destination--image");
+    v1 = document.getElementsByClassName("planetName");
+    v2 = document.getElementsByClassName("planetDescription");
+    v3 = document.getElementsByClassName("planetImage");
+    //
+    v6 = document.getElementsByClassName("planetImage2");
     v4 = document.getElementsByClassName("planetDistance");
     v5 = document.getElementsByClassName("planetTravel");
-    console.log(v1, v2, v3, v4, v5);
+    data_func(".destination", "Moon");
+    // console.log(v1, v2, v3, v4, v5);
   } else if (pageName === ".crew") {
     v1 = document.getElementsByClassName("crewMember--role");
     v2 = document.getElementsByClassName("crewMember--name");
-    v3 = document.getElementsByClassName("crewMember--description");
+    v3 = document.getElementsByClassName("crewMember--bio");
+    v4 = document.getElementsByClassName("crewMember--img");
   } else if (pageName === ".technology") {
-    console.log("fuck You !!");
+    v1 = document.getElementsByClassName("vehicle-type");
+    v2 = document.getElementsByClassName("technology--img");
+    v3 = document.getElementsByClassName("technology--description__meta");
+    console.log(v1, v2, v3);
   }
 }
